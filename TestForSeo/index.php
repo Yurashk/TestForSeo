2545286d82274d539f6d35ba34a550db4fd64734
Yurashk <Yurahi@ukr.net>
2018-09-06 20:42:45 +0300

    first commit

---
+++ TestForSeo/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Test</title>
+ </head>
+ <body>
+ <div class="container">
+	<header class="header">
+		<div><h4>Юридична Сотня допомогла <strong>29869 </strong> захисникамУкраїни</div>
+		<div ><button><strong>Зробити внесок</strong></button></div>
+		<div id="GarLin"><h4>Номер гарячої лінії: <strong>0 800 308 100</strong></h4></div>
+	</header>
+	<div class="Underheader"><div></div></div>
+	<nav class="navigation">
+		<div><img src="img/Logo.png" width="170" height="80" > </div>
+		
+		<div><a href="" class="active">Про нас</a></div>
+		<div><a href="" >Діяльність</a></div>
+		<div><a href="" >Учасникам війни</a></div>
+		<div><a href="" >Юристам</a></div>
+		<div><a href="" >Новини</a></div>
+		<div><a href="" >Події</a></div>
+		<div><a href="" >Контакти</a></div>
+		<div  id="find"><img src="img/vector-smart-object1.png" width="20" height="20" ></div>
+		<div>| УКР </div>
+	</ul>
+	</nav>
+	<main class="main">
+		<div class="Dopomoga">
+			<div id="left"><p><img src="img/handshake.png" width="150" height="150"><span><strong><p> <h3>Я хочу</h3></p><p><h3> отримати</h3></p><h3>допомогу</h3></strong> </span> </p></div>
+			<div id="right"><p><img src="img/palm.png" width="150" height="150"><span><strong><p> <h3>Я хочу</h3></p><p><h3> Надати</h3></p><h3>допомогу</h3></strong> </span></p></div>
+		</div>
+		<h2>Історії успіху </h2>
+		<div class="Historis"> 
+			<div><p><img src="img/soldat.png" width="270" height="270"> <h3>Тіріля Дмитро Юрійович</h3></p><p>Тіріля Дмитро Юрійович
+			-один з тих військовослужбовців ,хто</p><p> першим став на захист українських кордонів навесні 14-го</p>
+			<p>року.Дмитро протягом трьох років воював у складі 14 бригади</p>
+			<p>Збройних Сил...</p>
+			<button>Читати далі</button>
+			</div>
+		</div>
+		
+		<h2>Пам*ятки</h2> 
+		<div class="Pamyatku"> 
+			<div class="nested">
+				<div class="father" id="first"><p><img src="img/icons8-радар-24.png" width="45" height="45" ><span>Пам'яток 1 </span> </p> </div>
+				<div class="zavant">Завантажити PDF </div>
+				<div class="konsultatsii">Консультація online </div>
+			</div>
+			<div class="nested">
+				<div class="father" id="second"><p><img src="img/icons8-реклама-50.png" width="60" height="60" ><span>Пам'яток 2 </span>  </p> </div>
+				<div class="zavant">Завантажити PDF </div>
+				<div class="konsultatsii">Консультація online </div>
+			</div>
+			<div class="nested">
+				<div class="father" id="Three"><p><img src="img/icons8-безопасность-проверена-50.png" width="60" height="60" ><span>Пам'яток 3 </span> </p> </div>
+				<div class="zavant">Завантажити PDF </div>
+				<div class="konsultatsii">Консультація online </div>
+			</div>
+		</div>
+		<h2>Події</h2>
+		<div class="Podii">
+		
+			<div class="PodiiItem">
+				<div class="PodiiDate">22/03/2017-23/03/2018</div>
+				<div class="PodiiLine"></div>
+				<div class="PodiiInfo">Першим став на захист українських кордонів</div>
+			</div>
+			<div class="PodiiItem">
+				<div class="PodiiDate">22/03/2017-23/03/2018</div>
+				<div class="PodiiLine"></div>
+				<div class="PodiiInfo">Першим став ... </div>
+			</div>
+			<div class="PodiiItem">
+				<div class="PodiiDate">22/03/2017-23/03/2018</div>
+				<div class="PodiiLine"></div>
+				<div class="PodiiInfo">Першим став на захист українських кордонів</div>
+			</div>
+			<div class="PodiiItem">
+				<div class="PodiiDate">22/03/2017-23/03/2018</div>
+				<div class="PodiiLine"></div>
+				<div class="PodiiInfo">Першим став на захист українських</div>
+			</div>
+		</div>
+		<div class="InlButton"><button>Більше Подій</button></div>
+		<h2>Новини  </h2>
+		<div class="News">
+			<div class="NewsItem">
+				<div class="NewsImage"><img src="img/gorod.jpg" width="100%" ></div>
+				<div class="NewsZagolov">Першим став на захист українськихb</div>
+				<div class="NewsInfo">На Труханов остров и Гидропарк планируют провести канализацию</div>
+			</div>
+			<div class="NewsItem">
+				<div class="NewsImage"><img src="img/gorod1.jpg" width="100%" ></div>
+				<div class="NewsZagolov">Першим став на захист українськихb</div>
+				<div class="NewsInfo">На Труханов остров и Гидропарк планируют провести канализацию</div>
+			</div>
+			<div class="NewsItem">
+				<div class="NewsImage"><img src="img/gorod2.jpg" width="100%" ></div>
+				<div class="NewsZagolov">Першим став на захист українськихb</div>
+				<div class="NewsInfo">На Труханов остров и Гидропарк планируют провести канализацию планируют провести канализацию планируют провести канализацию</div>
+			</div>
+		</div>
+		
+	</main>
+	<div class="InlButton"><button>Більше новин</button></div>
+	<div class="upfooter">
+		<div></div>
+		<div>Підписатися на розсилку Юридичної Сотні <button><strong>Підписатися</strong></button></div>
+		<div></div>
+	</div>
+	<footer class="footer">
+		<div><img src="img/footer-logo.png" width="100%" ></div>
+		<div><a href="" class="active">Про нас</a></div>
+		<div><a href="" >Діяльність</a></div>
+		<div><a href="" >Учасникам війни</a></div>
+		<div><a href="" >Юристам</a></div>
+		<div><a href="" >Новини</a></div>
+		<div><a href="" >Події</a></div>
+		<div><a href="" >Контакти</a></div>
+		<div></div>
+	</footer>
+	
+</div>
+ </body>
+</html>
